cf1b4aae6a5e08c43761cf769b60149d6b47c553
Appealso <Appealso@naver.com>
2020-01-08 21:15:16 +0900

    Update boj1094_200108_KSH.java

--- Week2/boj1094_200108_KSH.java
+++ Week2/boj1094_200108_KSH.java
@@ -1,3 +1,4 @@
+//V1
 import java.util.*;
 
 public class boj1094_200108_KSH {
@@ -8,5 +9,16 @@ public class boj1094_200108_KSH {
 			if(X%2==1)cnt++;
 			X/=2;
 		}System.out.println(cnt);
+	}
+}
+
+//V2
+import java.util.*;
+
+public class boj1094_200108_KSH {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		System.out.println(Integer.bitCount(X));
 	}
 }
