4cd69c60f032857d947d4f40d4e9509f20b81b82
Appealso <Appealso@naver.com>
2020-01-08 21:11:59 +0900

    Create boj1094_200108_KSH.java

---
+++ Week2/boj1094_200108_KSH.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class boj1094_200108_KSH {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt(),cnt=0;
+		while(X!=0) {
+			if(X%2==1)cnt++;
+			X/=2;
+		}System.out.println(cnt);
+	}
+}
